296877a8aefb5cef456b8cfb50f42ff1d0d82bbb
Justin <justin@justinbennett.ca>
2018-12-23 16:05:00 -0500

    add main controller

---
+++ app/Http/Controllers/main_controller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DND_background;
+use App\DND_class;
+use App\DND_class_feature;
+use App\DND_race;
+use Illuminate\Http\Request;
+
+class main_controller extends Controller
+{
+    function index()
+    {
+
+        $race = DND_race::orderByRaw("RAND()")->first();
+        $class = DND_class::orderByRaw("RAND()")->first();
+        $class_feature = DND_class_feature::select('*')->where('base_class', '=', $class->name)->inRandomOrder()->first();
+        $background = DND_background::orderByRaw("RAND()")->first();
+
+        return view('char_roller', compact('race', 'class', 'background', 'class_feature'));
+
+    }
+}
